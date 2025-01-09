45c291ed1f153f0420d11b2dff28babec3851bf3
Sai Muralidhar <sai123murali@gmail.com>
2025-01-08 19:38:29 -0500

    updated end point

--- src/api/financialData.js
+++ src/api/financialData.js
@@ -1,11 +1,9 @@
 import axios from "axios";
 
-
-const BASE_URL = "http://127.0.0.1:5000/api/financial-data"; 
+const BASE_URL = "https://financial-data-app-4lt9.onrender.com/api/financial-data";  // Updated URL
 
 export const fetchCompanyIncomeStatements = async (symbol, filters) => {
   try {
-    // Construct query parameters based on filters
     const params = new URLSearchParams(filters).toString();
     const response = await axios.get(`${BASE_URL}/${symbol}?${params}`);
     return response.data;
